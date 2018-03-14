bf089035dfdc6ffbf44262ac3b20807643c32f9c
edikernel <edison.amg.95@gmail.com>
2018-03-14 20:33:55 +0000

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,1 +1,15 @@
-
+//Aquí invoco el paquete, el nombre del archivo
+package HolaMundo;
+import java.util.Scanner;
+//Aqui se genera la primera classe, la classe principal del paquete
+public class HolaMundo {
+  //Aquí se crean las variables estáticas
+  public static void main(String[] args) {
+    //Todo el código se ejecuta aquí
+    /**Creación de variables, repaso de datos primitivos
+    El tipo byte contiene números enteros, ocupan un byte.
+    Un byte són 8 bits*/
+    byte by = 0;
+    
+  }
+}
