282edc613612113e6241efa94cee714fc4692519
edikernel <edison.amg.95@gmail.com>
2018-03-20 20:39:10 +0000

    Update repeticioRepte1.java

--- repeticioRepte1.java
+++ repeticioRepte1.java
@@ -5,9 +5,17 @@ public class repeticioRepte1 {
     /////////////////////////////////////////////////////////////
     //Bloc de variables i subvariables pel tractament de dades//
     ///////////////////////////////////////////////////////////
+    
     boolean respostaOK = false;
     lector = new Scanner(System.in);
-    car = 0;
+    //variable que conte la resposta de l'usuari
+    int car = 0;
+    //variable que conté el contador
+    int contador = 0;
+    
+    ////////////////////////////////////////
+    //Bloc de preguntes i gestió d'errors//
+    //////////////////////////////////////
     
     //Pregunta a l'usuari quants caracters '-' vol que és repeteixi
     System.out.println("Cuants càracters "-" vols que és mostri per pantalla");
@@ -15,6 +23,7 @@ public class repeticioRepte1 {
     if (repostaOK) {
       car = lector.nextInt();
       lector.nextLine();
+      //fer un while, compara contador amb car, si és true
     }
     else {
       System.out.println("Les dades introduides no són correctes");
