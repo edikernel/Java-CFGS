6783d485ca3ddafe593e39a783f2057cc3b633ad
edikernel <edison.amg.95@gmail.com>
2018-03-22 20:54:09 +0000

    Update repeticioRepte2.java

--- repeticioRepte2.java
+++ repeticioRepte2.java
@@ -1,1 +1,9 @@
 package repeticiorepte2;
+import java.util.Scanner;
+public class repeticioRepte2 {
+  //Identificadores estáticos
+  
+  static void main (String [] args) {
+    //variables
+  }
+}
