f0c1eb89724b7918265b0805d63e470c2ffc466e
edikernel <edison.amg.95@gmail.com>
2018-04-24 13:59:55 +0200

    Update E12_MuñozG.java

--- E12_MuñozG.java
+++ E12_MuñozG.java
@@ -21,11 +21,16 @@ public class E12_MuñozG {
         ////////////
         
         Scanner teclado = new Scanner(System.in); //Assignar a la variable l'eina per introduïr dades pel teclat.
-        boolean userOK = false; //per controlar si el nom de l'usuari es correcte
         boolean tipusOK = false; //Per controlar si el tipus de dada es correcte
         boolean dadesOK = false;//Per controlar si compleix amb totes les condicions de la dada esperada.
         int numUsers = 0;//Numero per defecte d'usuaris
         String[] users; //array que contindrá el nom dels usuaris, encara no té la mesura de l'array.
+        String contrasenya; //contindrá la password generada
+        String userAux;
+        
+        //////////////////////////////////
+        //REGISTRE I GENERACIÓ PASSWORD//
+        ////////////////////////////////
         
         do {
             System.out.println("Quants usuaris vols registrar?");
@@ -46,8 +51,49 @@ public class E12_MuñozG {
                 System.out.println("______________________________________________________________");
                 System.out.println("Les dades introduïdes no son correctes, torna-ho a intentar-ho");
             }
-        } while (!dadesOK);
+        } while (!dadesOK);//si dadesOK es true surt del bucle
         users = new String [numUsers];//Indicar la mesura de l'array, la mesura segons les dades anteriors.
+        for (int contador = 0; contador < numUsers; contador++) {//si contador es més petit que la dada anterior... augmenta contador cada vegada
+            System.out.println("·····························");
+            System.out.println("Introdueix el nom de l'usuari");
+            users[contador] = teclado.nextLine();//array que el nom de tots els usuaris introduïts
+            //generació de contrasenya
+            contrasenya = users[contador].replace('a' , '4'); 
+            contrasenya = contrasenya.replace('e' , '4');
+            contrasenya = contrasenya.replace('i' , '4');
+            contrasenya = contrasenya.replace('o' , '4');
+            contrasenya = contrasenya.replace('u' , '4');
+            contrasenya = contrasenya.replace('A' , '4');
+            contrasenya = contrasenya.replace('E' , '4');
+            contrasenya = contrasenya.replace('I' , '4');
+            contrasenya = contrasenya.replace('O' , '4');
+            contrasenya = contrasenya.replace('U' , '4');
+            System.out.println("La contrasenya és:" +contrasenya );
+        }
+        
+        ////////////////////////
+        //LLISTAT I ORDENACIÓ//
+        //////////////////////
+        
+        System.out.println("______________________________________");
+        System.out.println("Llistat dels usuaris");
+        for (int contador = 0; contador < numUsers; contador++) { //genera bucle amb contador, contador ha de ser mes petit que numUsers
+            System.out.println(users[contador]);
+        }
+        for (int contador = 0; contador < users.length -1; contador++) {
+            for (int x = contador + 1; x < users.length; x++) {
+                if (users[contador].compareTo(users[x]) > 0) {
+                    userAux = users[contador];
+                    users[contador] = users[x];
+                    users[x] = userAux;
+                }
+            }
+        }
+        System.out.println("______________________________________");
+        System.out.println("Llistat ordenat dels usuaris");
+        for (int contador = 0; contador < numUsers; contador++) {
+            System.out.println(users[contador]);
+        }
         /**
         do {
             //System.out.println("-----------------------------");
