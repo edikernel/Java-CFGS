48d89f22459f483a543ca816326247cd9b78089b
edikernel <edison.amg.95@gmail.com>
2019-02-25 21:48:58 +0100

    Update A1IntroducirIPs.java

--- A1IntroducirIPs.java
+++ A1IntroducirIPs.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class A1IntroduirIPs {
-    /** Declareu en aquesta zona les constants i variables globals 
+    /** Declareu en aquesta zona les constant i variables globals 
      * accessibles des de totes les funcions i des del programa principal.
      */
     ///////////////
     //CONSTANSTS//
     /////////////
 
-    //determina el valor mÃ¡xim de la array, cuantes direccions es guardaran?
+    //determina el valor máxim de la array, cuantes direccions es guardaran?
     static final int IP_MAX = 1;
     //determina el valor mÃ¡xim de la array que guardara els numeros corresponents del rang de IP
     static final int IP_RANG = 3;
@@ -19,7 +19,7 @@ public class A1IntroduirIPs {
     //////////////
     //VARIABLES//
     ////////////
-    //DeclaraciÃ³ array on es guardara les dades introduides
+    //Declaració array on es guardara les dades introduides
     String[] ipIntroduida = new String[IP_MAX];
     //Declaració array on es guardará els diferents números dels 4 octets, cada octet correspon a un valor del array
     //String numerosIp[] = new String[IP_RANG];
@@ -41,16 +41,16 @@ public class A1IntroduirIPs {
     
     public static void main(String[] args) {
         // Es crea una variable prg de tipus A1IntroduirIPs i es crida al seu
-        // mÃƒÂ¨tode inici per a comenÃƒÂ§ar el programa.
+        // mètode inici per a començar el programa.
         A1IntroduirIPs prg = new A1IntroduirIPs();
         prg.inici();
     }
     
     /**
      * Programa principal. 
-     * Anirem demanant IP's fins que el nombre d'IPs introduÃƒÂ¯des siguin les que
-     * marca la constant del nostre programa, o bÃƒÂ© la IP introduÃƒÂ¯da sigui 0.0.0.0
-     * Una variable anirÃƒ  comptant el nombre d'IPs que s'han introduÃƒÂ¯t i indexant 
+     * Anirem demanant IP's fins que el nombre d'IPs introduïdes siguin les que
+     * marca la constant del nostre programa, o bé la IP introduïda sigui 0.0.0.0
+     * Una variable anirà comptant el nombre d'IPs que s'han introduït i indexant 
      * a la vegada l'array on aquestes es guarden.
      */
     public void inici(){
@@ -58,12 +58,12 @@ public class A1IntroduirIPs {
     	comprovarFormatIp();
     }
     
-    /** FunciÃƒÂ³ o mÃƒÂ¨tode que demana per teclat una IP i no en fa la comprovaciÃƒÂ³ de si ÃƒÂ©s o
-     * no correcte, nomÃƒÂ©s demana que s'introdueixi un text i el guarda a la variable
+    /** Funció o mètode que demana per teclat una IP i no en fa la comprovació de si és o
+     * no correcte, només demana que s'introdueixi un text i el guarda a la variable
      * global corresponent per a ser usada des del codi principal.
      */
     public void demanarIP(){
-    	//Sempre que x sigui igual o mÃ©s petit que IP_MAX fes... 
+    	//Sempre que x sigui igual o més petit que IP_MAX fes... 
     	for (int x = 0; x < IP_MAX; x++) {
             System.out.println("Introdueix una adreça IP valida");
             //escoltar les dades introduides pel teclat i guardar les dades en array
@@ -71,18 +71,18 @@ public class A1IntroduirIPs {
     	}
     }
     
-    /** FunciÃƒÂ³ o mÃƒÂ¨tode que recorre totes les posicions de l'arrayIps comprovant
-     * si alguna d'aquestes posicions contÃƒÂ© el mateix valor que el que es troba
-     * a la variable global que contÃƒÂ© la ip introduida, en cas de ser aixÃƒÂ­ 
-     * guardarÃƒ  en una variable global el valor de cert.
+    /** Funció o mètode que recorre totes les posicions de l'arrayIps comprovant
+     * si alguna d'aquestes posicions conté el mateix valor que el que es troba
+     * a la variable global que conté la ip introduida, en cas de ser així 
+     * guardarà en una variable global el valor de cert.
      */
     private void buscarIpsRepetides(){
 
     }
     
-    /** FunciÃƒÂ³ o mÃƒÂ¨tode que mitjanÃƒÂ§ant un split aplicat a l'String ipIntroduida
+    /** Funció o mètode que mitjançant un split aplicat a l'String ipIntroduida
      * separa aquesta en quatre parts diferents fent servir de separador el '.'
-     * En cas que tots els nÃƒÂºmeros obtinguts de les quatre parts estiguin entre
+     * En cas que tots els números obtinguts de les quatre parts estiguin entre
      * 0 i 255 fixa cert a la variable global ipCorrecta.
      */
     public void comprovarFormatIp(){
@@ -90,17 +90,15 @@ public class A1IntroduirIPs {
     	contComprovacio = 0;
     	//mentres ipcorrecta sigui fals i ademes el contador sigui més petit al tamany máxim de la array ipIntroduida...
     	while (!ipCorrecta && contComprovacio < ipIntroduida.length) {
-    		//crea un contador x per controlar la sortida de ipIntroduida, sempre que x sigui més petit que tamany máxim de la array ipIntroduida...
-            //for (int x = 0; x < ipIntroduida.length; x++) {
             	/**divideix les dades ipIntroduida separades pel "." i guarda les dades en la array numerosIp
             	 * El tamany de la array numerosIp es veura determinat segons les dades que s'hagin introduit previament
             	 */
                 String numerosIp[] = ipIntroduida[contComprovacio].split(Pattern.quote("."));
                 //si el tamany de la array es de 0 a 3 amb un total de 4 columnes ...
                 if ( numerosIp.length == 4 ){
-                	/**En les seguents 4 lieneas dona valor a les variables, indica que el valor era un objecte Integer
-                	 * i convertirá aquets objectes en tipus primitiu int
-                	 */
+                    /**En les seguents 4 lieneas dona valor a les variables, indica que el valor era un objecte Integer
+                     * i convertirá aquets objectes en tipus primitiu int
+                     */
                     num1 = Integer.parseInt(numerosIp[0]);
                     num2 = Integer.parseInt(numerosIp[1]);
                     num3 = Integer.parseInt(numerosIp[2]);
@@ -130,8 +128,7 @@ public class A1IntroduirIPs {
                 }
     	}       
     }
-    
-    /** FunciÃƒÂ³ o mÃƒÂ¨tode que recorre l'arrayIps per mostrar fila 
+    /** Funció o mètode que recorre l'arrayIps per mostrar fila 
      * a fila el seu contingut.
      */
     public void mostrarArrayIps(){
@@ -141,5 +138,4 @@ public class A1IntroduirIPs {
         	System.out.println("#################################################################");
         }
     }
-
 }
